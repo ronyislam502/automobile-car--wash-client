d0ee40c1f576d01bed9806e59e02caefd720cab6
ronyislam502 <ronyislam502@gmail.com>
2024-11-02 05:19:58 +0600

    route generator

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Banner from "./HomeCompo/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/pageRoutes.tsx
@@ -0,0 +1,8 @@
+import Home from "../pages/Home/Home";
+
+export const pageRoutes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
